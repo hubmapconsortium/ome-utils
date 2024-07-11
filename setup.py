4a2fb2b063dd4c697a7718c4821d8c4eb16ee9b6
Matt Ruffalo <matt.ruffalo@gmail.com>
2024-07-11 10:31:42 -0400

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(here / "README.rst", encoding="utf-8") as f:
 
 setup(
     name="hubmap-ome-utils",
-    version="0.2.0",
+    version="0.3.0",
     description="OME-XML utility functions for HuBMAP computational pipelines",
     long_description=long_description,
     long_description_content_type="text/x-rst",
